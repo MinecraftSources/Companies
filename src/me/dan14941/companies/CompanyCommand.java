563d0943cf8381ac7835b91d816a89ee9e808cfe
dan14941 <dan14941ac@gmail.com>
2014-04-11 16:41:41 +0800

    Fixed unused import

--- src/me/dan14941/companies/CompanyCommand.java
+++ src/me/dan14941/companies/CompanyCommand.java
@@ -1,6 +1,6 @@
 package me.dan14941.companies;
 
-import org.bukkit.Bukkit;
+//import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
